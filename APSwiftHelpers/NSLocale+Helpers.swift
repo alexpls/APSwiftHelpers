9ea25eec1abfcd70bc6af00299415f5728e72301
Alex Plescan <alexpls@gmail.com>
2016-02-29 17:44:40 +1000

    Added NSLocale helpers

---
+++ APSwiftHelpers/NSLocale+Helpers.swift
@@ -0,0 +1,24 @@
+//
+//  NSLocale+Helpers.swift
+//  APSwiftHelpers
+//
+//  Created by Alex Plescan on 29/02/2016.
+//  Copyright © 2016 Alex Plescan. All rights reserved.
+//
+
+import Foundation
+
+extension NSLocale {
+    
+    /// The country code for this locale. An example could be `US`.
+    public var countryCode: String? {
+        return objectForKey(NSLocaleCountryCode) as? String
+    }
+    
+    /// The country name for this locale. An example could be `United States`.
+    public var countryName: String? {
+        guard let countryCode = countryCode else { return nil }
+        return displayNameForKey(NSLocaleCountryCode, value: countryCode)
+    }
+    
+}
